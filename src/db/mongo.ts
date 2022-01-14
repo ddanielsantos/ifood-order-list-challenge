8c2de0d319cefefefbfc1be0bc24d8962cad1131
Daniel Santos <contatodanieljob@gmail.com>
2022-01-14 00:12:34 -0300

    adding MongoDB Client configuration file

---
+++ src/db/mongo.ts
@@ -0,0 +1,12 @@
+import { MongoClient } from "mongodb"
+import * as DotEnv from 'dotenv'
+
+DotEnv.config()
+
+const URL = process.env.DATABASE_URL
+const CLIENT = new MongoClient(URL)
+
+const DB_NAME = process.env.DATABASE_NAME
+const DB = CLIENT.db(DB_NAME)
+
+export { CLIENT, DB }
