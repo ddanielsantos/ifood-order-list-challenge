2b14aa9c885174645c44cd682587cda7f6bc8195
Daniel Santos <contatodanieljob@gmail.com>
2022-01-14 00:13:22 -0300

    adding index file and calling routes

---
+++ index.ts
@@ -0,0 +1,13 @@
+import Koa from 'koa'
+import * as dotenv from 'dotenv'
+import { router } from './src/routes/'
+
+dotenv.config()
+
+const app = new Koa()
+
+const PORT = process.env.SERVER_PORT
+
+app.use(router.routes())
+
+app.listen(PORT, () => console.log(`servidor rodando em 'http://localhost:${PORT}'`))
