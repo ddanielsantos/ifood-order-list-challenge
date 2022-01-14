920e5a92e1b701e38c0ebbcf61dffbde543b4c87
Daniel Santos <contatodanieljob@gmail.com>
2022-01-14 12:20:10 -0300

    connecting to remote database

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -4,7 +4,12 @@ import * as DotEnv from 'dotenv'
 DotEnv.config()
 
 const URL = process.env.DATABASE_URL || 'a'
-const CLIENT = new MongoClient(URL)
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || 'none'
+const FORMATTED_PASSWORD = URL.replace('<password>', DATABASE_PASSWORD)
+
+const FINAL_URL = process.env.ENVIRONMENT === 'development' ? URL : FORMATTED_PASSWORD
+
+const CLIENT = new MongoClient(FINAL_URL)
 
 const DB_NAME = process.env.DATABASE_NAME
 const DB = CLIENT.db(DB_NAME)
