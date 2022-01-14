1863882c2d5ca13827deaf130c171eb568529329
Daniel Santos <contatodanieljob@gmail.com>
2022-01-14 11:30:48 -0300

    preventing undefined instead of string

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -3,7 +3,7 @@ import * as DotEnv from 'dotenv'
 
 DotEnv.config()
 
-const URL = process.env.DATABASE_URL
+const URL = process.env.DATABASE_URL || 'a'
 const CLIENT = new MongoClient(URL)
 
 const DB_NAME = process.env.DATABASE_NAME
