ef13f1a027c41e8a3a5ebd42e5ed201e86360a20
Daniel Santos <contatodanieljob@gmail.com>
2022-01-18 19:30:16 -0300

    adding Koa Body Parser

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import Koa from 'koa'
+import bodyParser from 'koa-bodyparser'
 import dotenv from 'dotenv'
+
 import { router } from './src/routes/'
 
 dotenv.config()
@@ -8,6 +10,7 @@ const app = new Koa()
 
 const PORT = process.env.PORT || 3483
 
+app.use(bodyParser())
 app.use(router.routes())
 
 app.listen(PORT, () => console.log(`servidor rodando em 'http://localhost:${PORT}'`))
