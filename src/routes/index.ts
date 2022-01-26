707aff5789edd310e12cdb7ff112892fd097fdd9
Daniel Santos <contatodanieljob@gmail.com>
2022-01-26 00:58:38 -0300

    doesnt matter, its NoSql

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,11 @@
 import Router = require('@koa/router')
 
 import { order } from './order'
-import { client } from './client'
 
 const router = new Router()
 
 router.use(order.routes())
 router.use(order.allowedMethods())
-
-router.use(client.routes())
-router.use(client.allowedMethods())
 
 router.get('/', async (ctx, next) => {
   ctx.body = 'Ola'
